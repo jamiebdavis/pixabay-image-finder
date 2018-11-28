a41877af9fa71af85751fd57743f7e3ef008e66a
jamiebdavis <jamied1985@gmail.com>
2018-11-28 22:07:12 +0000

    rendered images

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -14,16 +14,22 @@ class Search extends Component {
     images: []
   };
   onTextChange = e => {
-    this.setState({ [e.target.name]: e.target.value }, () => {
-      axios
-        .get(
-          `${this.state.apiUrl}/?key=${this.state.apiKey}&q=${
-            this.state.searchText
-          }&image_type=photo&per_page=${this.state.amount}&safesearch=true`
-        )
-        .then(res => this.setState({ images: res.data.hits }))
-        .catch(err => console.log(err));
-    });
+    const val = e.target.value;
+
+    if (val === "") {
+      this.setState({ images: [] });
+    } else {
+      this.setState({ [e.target.name]: val }, () => {
+        axios
+          .get(
+            `${this.state.apiUrl}/?key=${this.state.apiKey}&q=${
+              this.state.searchText
+            }&image_type=photo&per_page=${this.state.amount}&safesearch=true`
+          )
+          .then(res => this.setState({ images: res.data.hits }))
+          .catch(err => console.log(err));
+      });
+    }
   };
   amountChange = (e, index, value) => {
     this.setState({ amount: value });
@@ -55,7 +61,7 @@ class Search extends Component {
         </Select>
         <br />
         {this.state.images.length > 0 ? (
-          <ImageResults images={this.state} />
+          <ImageResults images={this.state.images} />
         ) : null}
       </div>
     );
