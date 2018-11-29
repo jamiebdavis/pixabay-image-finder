8ec17affe5432d5567c22e619146ec7e77600235
Jamie Davis <jamied1985@gmail.com>
2018-11-29 10:42:13 +0000

    adjusted app bar

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -1,6 +1,14 @@
 import React from "react";
 import AppBar from "material-ui/AppBar";
+import { deepPurple50 } from "material-ui/styles/colors";
 
-const NavBar = () => <AppBar title="Pixabay Image Finder" />;
+const accent = deepPurple50;
+
+const NavBar = () => (
+  <AppBar
+    style={{ background: "#2E3B55", textAlign: "center" }}
+    title="Pixabay Image Finder"
+  />
+);
 
 export default NavBar;
